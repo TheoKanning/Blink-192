0bdfc9182c3b30a3a15e1df51d896f54d2694d8f
Theo Kanning <theokanning@gmail.com>
2018-11-22 13:09:42 -0600

    Publish last velocity at 10Hz

--- scripts/keys_to_twist.py
+++ scripts/keys_to_twist.py
@@ -11,13 +11,15 @@ key_mapping = {
         's': [0, 0]
     }
 
+twist = Twist()
+
 def keys_callback(msg, twist_pub):
+    global twist
     if len(msg.data) == 0 or not key_mapping.has_key(msg.data[0]):
         # unrecognized key
         return
 
     velocities = key_mapping[msg.data[0]]
-    twist = Twist()
     twist.angular.z = velocities[0]
     twist.linear.x = velocities[1]
     twist_pub.publish(twist)
@@ -27,4 +29,7 @@ if __name__ == '__main__':
     rospy.init_node('keys_to_twist')
     twist_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
     rospy.Subscriber('keys', String, keys_callback, twist_pub)
-    rospy.spin()
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        twist_pub.publish(twist)
+        rate.sleep()
