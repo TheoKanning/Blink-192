9a390442722fc7f30e1631ea51fbef4ac191bb36
Theo Kanning <theokanning@gmail.com>
2018-11-22 13:03:14 -0600

    Add basic keys_to_twist node

---
+++ scripts/keys_to_twist.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+
+key_mapping = {
+        'w': [0, 1],
+        'x': [0, -1],
+        'a': [-1, 0],
+        'd': [1, 0],
+        's': [0, 0]
+    }
+
+def keys_callback(msg, twist_pub):
+    if len(msg.data) == 0 or not key_mapping.has_key(msg.data[0]):
+        # unrecognized key
+        return
+
+    velocities = key_mapping[msg.data[0]]
+    twist = Twist()
+    twist.angular.z = velocities[0]
+    twist.linear.x = velocities[1]
+    twist_pub.publish(twist)
+
+if __name__ == '__main__':
+    print "Starting keys_to_twist"
+    rospy.init_node('keys_to_twist')
+    twist_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+    rospy.Subscriber('keys', String, keys_callback, twist_pub)
+    rospy.spin()
