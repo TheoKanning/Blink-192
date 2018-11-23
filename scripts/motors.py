4a74c265e098085d7af514f62fae03b45c6a928a
Theo Kanning <theokanning@gmail.com>
2018-11-23 14:01:23 -0600

    Add motors node

---
+++ scripts/motors.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+from alphabot.alphabot import AlphaBot
+
+speed = 50
+differential = 25
+bot = AlphaBot()
+
+def command_callback(twist):
+    left_speed = speed * twist.linear.x + differential * twist.angular.z
+    right_speed = speed * twist.linear.x - differential * twist.angular.z
+    bot.setMotor(left_speed, right_speed)
+
+if __name__ == '__main__':
+    print "Starting motor node"
+    rospy.init_node('motors')
+    rospy.Subscriber('cmd_vel', Twist, command_callback)
+    rospy.spin()
